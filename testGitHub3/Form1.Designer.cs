5968c751b37c583f56a79581b57cf726379e61c5
Yvan Morrissey <Yvan.Morrissey@cegeplevis.ca>
2021-09-13 08:47:44 -0400

    changements de plus API win

--- testGitHub3/Form1.Designer.cs
+++ testGitHub3/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace testGitHub3
 {
-    partial class Form1
+    partial class frmTest
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,34 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnTestAPI_Win = new System.Windows.Forms.Button();
+            this.btnTest2API_Win = new System.Windows.Forms.Button();
+            this.btn3API_Win = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnTest
             // 
-            this.button1.Location = new System.Drawing.Point(91, 75);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnTest.Location = new System.Drawing.Point(39, 35);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(163, 23);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "btnTest";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
             // 
-            // Form1
+            // btnTestAPI_Win
+            // 
+            this.btnTestAPI_Win.Location = new System.Drawing.Point(39, 80);
+            this.btnTestAPI_Win.Name = "btnTestAPI_Win";
+            this.btnTestAPI_Win.Size = new System.Drawing.Size(163, 23);
+            this.btnTestAPI_Win.TabIndex = 1;
+            this.btnTestAPI_Win.Text = "btnTestAPI_Win";
+            this.btnTestAPI_Win.UseVisualStyleBackColor = true;
+            this.btnTestAPI_Win.Click += new System.EventHandler(this.btnTestAPI_Win_Click);
+            // 
+            // btnTest2API_Win
+            // 
+            this.btnTest2API_Win.Location = new System.Drawing.Point(39, 128);
+            this.btnTest2API_Win.Name = "btnTest2API_Win";
+            this.btnTest2API_Win.Size = new System.Drawing.Size(163, 23);
+            this.btnTest2API_Win.TabIndex = 2;
+            this.btnTest2API_Win.Text = "btnTest 2 API_Win";
+            this.btnTest2API_Win.UseVisualStyleBackColor = true;
+            this.btnTest2API_Win.Click += new System.EventHandler(this.btnTest2API_Win_Click);
+            // 
+            // btn3API_Win
+            // 
+            this.btn3API_Win.Location = new System.Drawing.Point(45, 182);
+            this.btn3API_Win.Name = "btn3API_Win";
+            this.btn3API_Win.Size = new System.Drawing.Size(157, 23);
+            this.btn3API_Win.TabIndex = 3;
+            this.btn3API_Win.Text = "btn3API_Win";
+            this.btn3API_Win.UseVisualStyleBackColor = true;
+            this.btn3API_Win.Click += new System.EventHandler(this.btn3API_Win_Click);
+            // 
+            // frmTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(369, 225);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(465, 251);
+            this.Controls.Add(this.btn3API_Win);
+            this.Controls.Add(this.btnTest2API_Win);
+            this.Controls.Add(this.btnTestAPI_Win);
+            this.Controls.Add(this.btnTest);
+            this.Name = "frmTest";
+            this.Text = "Test API windows";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnTestAPI_Win;
+        private System.Windows.Forms.Button btnTest2API_Win;
+        private System.Windows.Forms.Button btn3API_Win;
     }
 }
 
